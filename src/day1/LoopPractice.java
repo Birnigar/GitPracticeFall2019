ca735fdf1b7ae57846cdd413e7e1b7b5e7e229b9
Birnigar <farukgalatasaray34@gmail.com>
2020-02-29 21:50:35 -0800

    loop practice

---
+++ src/day1/LoopPractice.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+         String str="abc";
+
+        for (int i = 0; i <str.length() ; i++) {
+            System.out.println(str.charAt(i));
+
+        }
+    }
+}
